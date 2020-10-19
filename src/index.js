ae17684cd9569e3b8f4f9ace0c0fe3e70ed3ba01
Yuliana Polukhina <polushka.yulia@gmail.com>
2020-10-19 22:52:48 +0300

    feat: add checkbrackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let l = bracketsConfig.length;
+  let newstr = '';
+  let arr = '';
+
+  for (let i = 0; i < str.length; i++) {
+      if ((str[i] === ')' && str[i + 1] === '(')
+       ||(str[i] === ']' && str[i + 1] === '(')) {
+          newstr += str[i];
+          newstr += ' ';
+      }
+      else {
+          newstr += str[i];
+      }
+  }
+
+  if (newstr.includes(' ')) {
+      arr = newstr.substring(' ');
+
+      for (let i = 0; i < l; i++) {
+          let el = bracketsConfig[i].join('');
+          for (let i = 0; i < arr.length; i++) {
+              if (el.includes(arr[i])) {
+                  return true;
+              }
+          }
+          return false;
+      }
+  }
+  else {
+      arr = newstr;
+      for (let i = 0; i < l; i++) {
+          let el = bracketsConfig[i].join('');
+          if (el.includes(arr[i])) {
+              return true;
+          }
+          return false;
+      }
+  }
+
+
+
 }
